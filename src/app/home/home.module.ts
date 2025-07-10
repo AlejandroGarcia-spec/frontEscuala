64f45ba958f979f60adf206279ed56d78d53f178
Oswaldo Hernández Sabah <149078668+oshersa-170701@users.noreply.github.com>
2025-07-10 08:44:57 -0600

    AGEGADO FOOTER EN HTML DE HOME E IMPORTACION DEL MODULO DE FOOTER EN TS DE HOME

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { FooterPage } from "../componentes/footer/footer.page";
 
 
 @NgModule({
@@ -12,8 +13,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
-  ],
+    HomePageRoutingModule,
+    FooterPage
+],
   declarations: [HomePage]
 })
 export class HomePageModule {}
