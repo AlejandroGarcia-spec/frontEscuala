f1b4a76c6ccdded743dfddf427b692230f9758f8
Oswaldo Hernández Sabah <149078668+oshersa-170701@users.noreply.github.com>
2025-07-11 13:36:34 -0600

    Se modificaron los ts y el module de los demas modales

--- src/app/modal/agregar-maestro-modal/agregar-maestro-modal.page.ts
+++ src/app/modal/agregar-maestro-modal/agregar-maestro-modal.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-agregar-maestro-modal',
+  standalone: true,
+  imports: [IonicModule],
   templateUrl: './agregar-maestro-modal.page.html',
   styleUrls: ['./agregar-maestro-modal.page.scss'],
 })
-export class AgregarMaestroModalPage implements OnInit {
+export class AgregarMaestroModalPage{
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
--- src/app/modal/editar-maestro-modal/editar-maestro-modal.page.ts
+++ src/app/modal/editar-maestro-modal/editar-maestro-modal.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-editar-maestro-modal',
+  standalone: true,
+  imports: [IonicModule],
   templateUrl: './editar-maestro-modal.page.html',
   styleUrls: ['./editar-maestro-modal.page.scss'],
 })
-export class EditarMaestroModalPage implements OnInit {
+export class EditarMaestroModalPage  {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
