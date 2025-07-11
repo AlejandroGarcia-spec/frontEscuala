bc2564447e343d04c2a4b8c59af510b13e2363fd
Oswaldo Hernández Sabah <149078668+oshersa-170701@users.noreply.github.com>
2025-07-10 18:20:16 -0600

    Se modifico la plantilla de perfil admin

--- src/app/pages/perfiles/perfil-admin/perfil-admin.page.ts
+++ src/app/pages/perfiles/perfil-admin/perfil-admin.page.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ToastController } from '@ionic/angular';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { FooterPage } from "src/app/componentes/footer/footer.page";
 
 @Component({
   selector: 'app-perfil-admin',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, FooterPage],
   templateUrl: './perfil-admin.page.html',
   styleUrls: ['./perfil-admin.page.scss'],
 })
 export class PerfilAdminPage {
+ nombre: string = "";
+  tipo_usuario: string = "";
+  telefono: string = "";
 
-  constructor() { }
+  constructor(private authService: AuthService, private toastController: ToastController) {}
 
+  ngOnInit() {
+    //this.cargarAdminAutenticado();
+  }
 
+  /* cargarAdminAutenticado() {
+    const admin = this.authService.getUserData3();
+   // console.log('Admin from localStorage:', admin); // Log para verificar los datos en localStorage
+    if (admin && admin.id) {
+      this.authService.getAdminById(admin.id).subscribe(
+        (resp: any) => {
+       //   console.log('Response from API:', resp); // Log para verificar la respuesta de la API
+          this.nombre = resp.nombre;
+          this.tipo_usuario = resp.tipo_usuario;
+          this.telefono = resp.telefono;
+        },
+        (error) => {
+          //console.error('Error al obtener la información del usuario:', error);
+          this.presentToast('Error al obtener la información del administrador');
+        }
+      );
+    } else {
+      //console.error('No se pudo obtener la información del usuario');
+      this.presentToast('No se pudo obtener la información del administrador');
+    }
+  }
+*/
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000,
+      position: 'top',
+      color: 'danger'
+    });
+    await toast.present();
+  }
 }
