3e0b1c2a452a77b3d851ed0f97ab077946ed9bb4
Oswaldo Hernández Sabah <149078668+oshersa-170701@users.noreply.github.com>
2025-07-15 18:58:22 -0600

    SE AGREGO HTML Y PARTE DE TS, AH LOS MODALES ELIMINAR Y EDITAR

--- src/app/modal/editar-grupo-modal/editar-grupo-modal.page.ts
+++ src/app/modal/editar-grupo-modal/editar-grupo-modal.page.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonicModule, ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-editar-grupo-modal',
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, ReactiveFormsModule,CommonModule,FormsModule],
   templateUrl: './editar-grupo-modal.page.html',
   styleUrls: ['./editar-grupo-modal.page.scss'],
 })
 export class EditarGrupoModalPage  {
 
-  constructor() { }
+ nombre: string = '';
+  id: number = 0;
+  carreraId: number = 0;
+  grupoCarreraId: number = 0; // Nueva propiedad para almacenar el ID del grupo seleccionado
+  carreras: any[] = [];
+  grupos: any[] = [];
+  selectedGrupoId: string='';
+  selectedCarreraId: string = '';
+  constructor(private modalController: ModalController, private toastController: ToastController) {
 
+      this.id = 0;
+      this.carreraId = 0;
+      this.grupoCarreraId = 0;
+  }
 
+  onCarreraChange(event: any) {
+}
+
+modificarGrupo() {
+
+  }
+
+  cerrarModal() {
+    this.modalController.dismiss();
+  }
 }
--- src/app/modal/eliminar-grupo-modal/eliminar-grupo-modal.page.ts
+++ src/app/modal/eliminar-grupo-modal/eliminar-grupo-modal.page.ts
@@ -1,16 +1,69 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AlertController, IonicModule, ModalController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-eliminar-grupo-modal',
-  imports: [IonicModule],
+  imports: [IonicModule, FormsModule,ReactiveFormsModule,CommonModule],
   standalone: true,
   templateUrl: './eliminar-grupo-modal.page.html',
   styleUrls: ['./eliminar-grupo-modal.page.scss'],
 })
 export class EliminarGrupoModalPage  {
+  eliminarForm!: FormGroup;
+  carreras: any[] = [];
+  grupos: any[] = [];
+constructor(
+    private modalController: ModalController,
+    private alertController: AlertController,
+    private toastController: ToastController,
+    private formBuilder: FormBuilder
+  ) {}
 
-  constructor() { }
+  ngOnInit() {
+    this.loadCarreras();
+    this.eliminarForm = this.formBuilder.group({
+      selectedCarreraId: ['', Validators.required],
+      grupoCarreraId: ['', Validators.required]
+    });
+  }
 
+  loadCarreras() {
 
+  }
+
+  onCarreraChange(event: any) {
+
+  }
+
+  async confirmarEliminacion() {
+    const alert = await this.alertController.create({
+      header: 'Confirmar Eliminación',
+      message: '¿Estás seguro de que deseas eliminar este grupo?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary'
+        },
+        {
+          text: 'Eliminar',
+          handler: () => {
+            this.eliminarGrupo(this.eliminarForm.get('grupoCarreraId')?.value);
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  eliminarGrupo(id: number) {
+    
+  }
+
+  cerrarModal() {
+    this.modalController.dismiss();
+  }
 }
